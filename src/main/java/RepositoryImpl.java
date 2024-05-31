90c855eeba3dff11e8df5400c80951546d035616
Ahmet Beşkazalıoğlu <ahmetbeskazalioglu@icloud.com>
2024-05-31 22:53:20 +0300

    some changes

--- src/main/java/RepositoryImpl.java
+++ src/main/java/RepositoryImpl.java
@@ -1,9 +1,8 @@
 import org.springframework.stereotype.Repository;
 
-import java.lang.annotation.Annotation;
-
 @Repository // Bu sınıfın bir bileşen olduğunu belirtiyoruz
 public class RepositoryImpl implements IRepository {
+
     @Override
     public void performDatabaseOperations() {
         System.out.println("Performing database operations...");
